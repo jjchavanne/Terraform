59b8f7cd327af8c94bd93a4e60667eeec938567f
jjchavanne <31355989+jjchavanne@users.noreply.github.com>
2019-10-02 14:30:23 -0400

    Create main.tf

---
+++ Instances/main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  region = "${var.region}"
+}
+resource "google_compute_instance" "default" {
+  project      = "${var.project_name}"
+  name         = "debian"
+  machine_type = "n1-standard-1"
+  zone         = "${var.zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
