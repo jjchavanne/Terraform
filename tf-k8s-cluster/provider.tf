1d0bcd85e889d15505085319172fba53a182f47a
jjchavanne <31355989+jjchavanne@users.noreply.github.com>
2020-02-01 13:25:08 -0500

    added separate node pool

--- tf-k8s-cluster/provider.tf
+++ tf-k8s-cluster/provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   #  credentials = "${file("./creds/serviceaccount.json")}"
-  project   = "${var.project_name}"
-  region    = "${var.region}"
-  zone      = "${var.zone}"
+  project = "${var.project_name}"
+  region  = "${var.region}"
+  zone    = "${var.zone}"
 }
