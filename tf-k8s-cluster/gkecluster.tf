1ea5378044d45231128ad3ae1ff198f8ff0cebf3
jjchavanne <31355989+jjchavanne@users.noreply.github.com>
2020-02-01 14:34:40 -0500

    updated k8s-cluster node-pool

--- tf-k8s-cluster/gkecluster.tf
+++ tf-k8s-cluster/gkecluster.tf
@@ -1,24 +1,24 @@
 resource "google_container_cluster" "gke-cluster" {
-  name               = "my-cluster1"
-  network            = "default"
-  location           = "${var.zone}"
+  name                     = "my-cluster1"
+  network                  = "default"
+  location                 = "${var.zone}"
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  master_auth {
-    username = ""
-    password = ""
+  #  master_auth {
+  #    username = ""
+  #    password = ""
 
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
+  #    client_certificate_config {
+  #      issue_client_certificate = false
+  #    }
+  #  }
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   location   = "${var.zone}"
-  cluster    = google_container_cluster.primary.name
+  cluster    = google_container_cluster.gke-cluster.name
   node_count = 1
 
   node_config {
@@ -29,12 +29,12 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       disable-legacy-endpoints = "true"
     }
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    #    oauth_scopes = [
+    #      "https://www.googleapis.com/auth/logging.write",
+    #      "https://www.googleapis.com/auth/monitoring",
+    #    ]
   }
 }
 
-# Need to enable this as well
-# gcloud config set compute/zone us-central1-b
+# Temporarily need to enable this as well
+# gcloud container clusters get-credentials my-cluster1
